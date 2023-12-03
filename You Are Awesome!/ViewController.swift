512a949989c35c189b042086ad0cb25b01ea2657
Harmandeep Gill <gillhimmy@gmail.com>
2023-12-03 15:31:40 -0500

    Chapter 2.16 video lesson complete Application now plays non repeating random sounds on "Show Message" button click Refactored code

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -14,12 +14,35 @@ class ViewController: UIViewController {
     
     var imageNumber = -1
     var messageNumber = -1
+    var soundNumber = -1
     let totalNumberOfImages = 9
+    let totalNumberOfSounds = 6
     var audioPlayer: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         messageLabel.text = ""
+    }
+    
+    func playSound(name: String) {
+        if let sound = NSDataAsset(name: name) {
+            do {
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            } catch {
+                print("😡 ERROR: \(error.localizedDescription) Could not intialize AVAudioPlayer object.")
+            }
+        } else {
+            print("😡 ERROR: Could not read data from file \(name)")
+        }
+    }
+    
+    func nonRepeatingRandom(originalNumber: Int, upperLimit: Int) -> Int {
+        var newNumber: Int
+        repeat {
+             newNumber = Int.random(in: 0...upperLimit)
+        } while newNumber == originalNumber
+        return newNumber
     }
     
     @IBAction func messageButtonPressed(_ sender: UIButton) {
@@ -30,29 +53,13 @@ class ViewController: UIViewController {
                         "Fabulous? That's You!",
                         "You've Got the Design Skills of Jony Ive"]
         
-        var newMessageNumber: Int
-        repeat {
-             newMessageNumber = Int.random(in: 0...messages.count-1)
-        } while messageNumber == newMessageNumber
-        messageNumber = newMessageNumber
+        var messageNumber = nonRepeatingRandom(originalNumber: messageNumber, upperLimit: messages.count-1)
         messageLabel.text = messages[messageNumber]
         
-        var newImageNumber: Int
-        repeat {
-            newImageNumber = Int.random(in: 0...totalNumberOfImages)
-        } while imageNumber == newImageNumber
-        imageNumber = newImageNumber
+        var imageNumber = nonRepeatingRandom(originalNumber: imageNumber, upperLimit: totalNumberOfImages-1)
         imageView.image = UIImage(named: "image\(imageNumber)")
         
-        if let sound = NSDataAsset(name: "sound0") {
-            do {
-                try audioPlayer = AVAudioPlayer(data: sound.data)
-                audioPlayer.play()
-            } catch {
-                print("😡 ERROR: \(error.localizedDescription) Could not read data from file sound0")
-            }
-        } else {
-            print("😡 ERROR: Could not read data from file sound0")
-        }
+        var soundNumber = nonRepeatingRandom(originalNumber: soundNumber, upperLimit: totalNumberOfSounds-1)
+        playSound(name: "sound\(soundNumber)")
     }
 }
